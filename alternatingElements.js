21b9924f4640a04a3b7aafa15f78bf86443b8319
Alex <alexandralaudon@gmail.com>
2015-06-02 10:59:39 -0600

    adding alternatingElements between two array

--- alternatingElements.js
+++ alternatingElements.js
@@ -1,10 +1,11 @@
 // Write a function, alternatingElements, that combines two lists by alternatingly taking elements:
 
 var alternatingElements = function(array1,array2) {
+  var max = Math.max(array1.length, array2.length);
   var combinedResult = [];
-  for (var i = 0; i < array1.length; i++) {
-    combinedResult.push(array1[i]);
-    combinedResult.push(array2[i]);
+  for (var i = 0; i < max; i++) {
+    if (array1[i] !== undefined) combinedResult.push(array1[i]);
+    if (array1[i] !== undefined) combinedResult.push(array2[i]);
   }
   return combinedResult;
 };
